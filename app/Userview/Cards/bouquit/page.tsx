03e847da5bc0d672b0c7d8ef71652f0008355b2e
zada-ai <sohaibmehmood0067@gmail.com>
2025-12-20 16:13:15 +0500

    ?

--- app/Userview/Cards/bouquit/page.tsx
+++ app/Userview/Cards/bouquit/page.tsx
@@ -43,7 +43,7 @@ export default function BouquitSection() {
   const visibleCandles = bouquets.slice(currentIndex, currentIndex + 4);
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/Bouquets.php")
+    fetch("https://rizaglowgardens.xo.je/Backend1/api/Bouquets.php")
       .then((res) => res.json())
       .then((data) => {
         if (data.error) {
